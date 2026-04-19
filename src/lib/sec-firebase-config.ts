88c1ee4ed9a27b1d0ee56b5843b0758ba9917d89
Brian <briankariuki9975@gmail.com>
2026-04-19 16:41:21 +0300

    imported environment variables in the sec-firebase-config

--- src/lib/sec-firebase-config.ts
+++ src/lib/sec-firebase-config.ts
@@ -1,56 +1,61 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, deleteApp } from "firebase/app";
 import { getFirestore, setDoc, doc, serverTimestamp } from "firebase/firestore";
-import { getAuth, signOut, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { toast } from "sonner";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqF5fDT2OVj92HK1LZLkBlEeyb4irbn4Q",
-  authDomain: "strathmore-chaplaincy-library.firebaseapp.com",
-  projectId: "strathmore-chaplaincy-library",
-  storageBucket: "strathmore-chaplaincy-library.firebasestorage.app",
-  messagingSenderId: "12340744601",
-  appId: "1:12340744601:web:1d055bd958d78920204308",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Function to add new admins
 let secApp;
- export const addNewAdmin = async (email: string, pass:string, name: string, setOpen: React.Dispatch<React.SetStateAction<boolean>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>, setAlert: React.Dispatch<React.SetStateAction<boolean>>) => {
-setLoading(true)
-    secApp = initializeApp(firebaseConfig, "SecondaryAppInstance");
+export const addNewAdmin = async (
+  email: string,
+  pass: string,
+  name: string,
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  setAlert: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  setLoading(true);
+  secApp = initializeApp(firebaseConfig, "SecondaryAppInstance");
 
- const secDb = getFirestore(secApp);
- const secAuth = getAuth(secApp);
+  const secDb = getFirestore(secApp);
+  const secAuth = getAuth(secApp);
 
   try {
+    const { user } = await createUserWithEmailAndPassword(secAuth, email, pass);
 
-  
-   
-    const { user } = await createUserWithEmailAndPassword(secAuth, email, pass)
-    
     await setDoc(doc(secDb, "users", user.uid), {
       name: name,
       email: email,
       role: "admin",
-      createdAt: serverTimestamp()
-    })
-    
-    
-    await signOut(secAuth)
-    await deleteApp(secApp)
-    setAlert(true)
+      createdAt: serverTimestamp(),
+    });
+
+    await signOut(secAuth);
+    await deleteApp(secApp);
+    setAlert(true);
     setTimeout(() => {
-      setAlert(false)
-    }, 6000)
-  }
-    
-   catch (error) {
- toast.error(`Error while adding Admin `)
+      setAlert(false);
+    }, 6000);
+  } catch (error) {
+    toast.error(`Error while adding Admin `);
   } finally {
-    setLoading(false)
-    setOpen(false)
+    setLoading(false);
+    setOpen(false);
   }
-}
+};
